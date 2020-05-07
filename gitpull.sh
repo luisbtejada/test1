23027847e72ff6fb2fbc7718b2ee4fdd309c6c4e
luisbtejada <luis.blanco.tejada@ericsson.com>
2020-05-07 11:41:10 +0000

    Thu May  7 11:41:09 2020

--- gitpull.sh
+++ gitpull.sh
@@ -1,2 +1,15 @@
+NOW=$(date +"%c")
+git add usr
+git add bin
+git add dev
+git add etc
+git add lib
+git add lib64
+git add root
+git add run
+git add sbin
+git add var
+git commit -a -m "$NOW"
 git fetch https://luisbtejada:Volando1!@github.com/luisbtejada/test1.git
 git pull https://luisbtejada:Volando1!@github.com/luisbtejada/test1.git
+git push https://luisbtejada:Volando1!@github.com/luisbtejada/test1.git
